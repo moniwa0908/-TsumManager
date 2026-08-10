f6cc31ad3a4f0335ee8f0a684e357b74b10859e7
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-10 22:37:16 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1574,7 +1574,7 @@ $("#clearRecentButton").onclick=()=>{recent=[];saveRecent();renderHome();toast("
 $("#exportButton").onclick=()=>{
   const backup={
     app:"TsumManager",
-    version:"8.4.13 Near Skillmax Removed",
+    version:"8.4.14 Input Simplified",
     backupType:"light",
     exportedAt:new Date().toISOString(),
     userData:buildStableUserStore(),
