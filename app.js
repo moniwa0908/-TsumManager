2e0f644a7c7858829c7adfbe677f244b98ff1d54
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-11 09:58:22 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1574,7 +1574,7 @@ $("#clearRecentButton").onclick=()=>{recent=[];saveRecent();renderHome();toast("
 $("#exportButton").onclick=()=>{
   const backup={
     app:"TsumManager",
-    version:"8.4.18 Progress Management Removed",
+    version:"8.4.19 Stable Status Removed",
     backupType:"light",
     exportedAt:new Date().toISOString(),
     userData:buildStableUserStore(),
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="tsum-manager-8-4-18-progress-management-removed-20260811-01";
+const CACHE="tsum-manager-8-4-19-stable-status-removed-20260811-01";
 const ASSETS=["./","./index.html","./style.css","./app.js","./tsums-data.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
