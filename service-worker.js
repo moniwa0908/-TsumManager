efebeef3dae06780a2ec8ee4ff97db56a8c5714f
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-16 17:14:33 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="tsum-manager-8-4-34-game8-skill-data-fix-20260816-01";
+const CACHE="tsum-manager-8-4-38-daruma-minnie-fix-20260816-01";
 const ASSETS=["./","./index.html","./style.css","./app.js","./tsums-data.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
